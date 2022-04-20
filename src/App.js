1a0cf63ef807f1bbe83cf95dde1e75e7bb799c14
MAYANK  TRIPATHI <mayank.tripathi@indianic.com>
2022-04-20 12:27:55 +0530

    dynamic-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+
+const [formfield, setformfield] = useState([{
+  name : '' , age : ''},
+])
+
+const handleFormChange = (event,index)=> {
+  let data = [...formfield];
+  data[index][event.target.name] = [event.target.value];
+  setformfield(data);
+}
+const submit = (e) => {
+  e.preventDefault();
+  console.log(formfield);
+}
+
+const addfield = ()=> {
+  let object = {
+    name : '',
+    age : ''
+  }
+  setformfield([...formfield,object])
+}
+
+const removefield = (index)=>{
+  let data = [...formfield];
+  data.splice(index,1);
+  setformfield(data)
+}
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={submit}>
+     {formfield.map((form,index) => {
+      return (
+        <div key = {index}>
+          <input
+            name='name'
+            placeholder='Name'
+            onChange = {event => handleFormChange(event,index)}
+            value = {form.name}
+          />
+          <input
+            name='age'
+            placeholder='Age'
+            onChange = {event => handleFormChange(event,index)}
+            value = {form.age}
+          />
+          <button onClick={() => removefield(index)}>Remove</button>
+        </div>
+      )
+    })}
+    
+      </form> 
+      <button onClick={addfield}>Add More..</button>
+      <br />
+      <button onClick={submit}>Submit</button>
     </div>
   );
 }
